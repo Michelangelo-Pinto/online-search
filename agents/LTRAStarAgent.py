35729a87e2216c30cc6719969a5232b34895ed9f
Michelangelo Pinto <141494163+Michelangelo-Pinto@users.noreply.github.com>
2024-10-03 01:46:49 +0200

    Update LTRAStarAgent.py

--- agents/LTRAStarAgent.py
+++ agents/LTRAStarAgent.py
@@ -6,10 +6,10 @@ class LRTAStarAgent:
     Takes a OnlineSearchProblem [Figure 4.23] as a problem.
     """
 
-    def __init__(self, problem):
+    def __init__(self, problem, H={}):
         self.problem = problem
         # self.result = {}      # no need as we are using problem.result
-        self.H = {}
+        self.H = H
         self.s = None
         self.a = None
 
@@ -36,59 +36,22 @@ class LRTAStarAgent:
             self.a = min(self.problem.actions(s1),
                          key=lambda b: self.problem.agent_cost_evaluation_function(self.problem, s1, b, self.problem.output(s1, b), self.H))
 
-            print(f"I chose to go in: {self.a()} and I think i will be this far from goal: {self.H[s1]}")
+            #DEBUG#print(f"I chose to go in: {self.a()} and I think i will be this far from goal: {self.H[s1]}")
 
             self.s = s1
             return self.a
 
 
 
-class MazeLRTAStarAgent(LRTAStarAgent):
-    def __init__(self, problem):
-        self.problem = problem
-        # self.result = {}      # no need as we are using problem.result
-        self.H = {}
-        self.s = None
-        self.a = None
-
-    def __call__(self, s1):  # as of now s1 is a state rather than a percept
-        if self.problem.goal_test(s1):
-            self.a = None
-            return self.a
-        else:
-            if s1 not in self.H:
-                self.H[s1] = self.problem.h(s1)
-            else:
-                self.H[s1] = float('inf')
-            
-            print(f"i am here: {s1} and i think i am this far from goal: {self.H[s1]}")
-
-                                
-            if self.s is not None:
-                # self.result[(self.s, self.a)] = s1    # no need as we are using problem.output
-            
-                # minimum cost for action b in problem.actions(s)
-                self.H[self.s] = min(self.LRTA_cost(self.s, b, self.problem.output(self.s, b),
-                                                    self.H) for b in self.problem.actions(self.s))
-
-            # an action b in problem.actions(s1) that minmizes costs
-            self.a = min(self.problem.actions(s1),
-                         key=lambda b: self.LRTA_cost(s1, b, self.problem.output(s1, b), self.H))
-
-            print(f"I chose to go in: {self.a()} and I think i will be this far from goal: {self.H[s1]}")
-
-            self.s = s1
-            return self.a
-
-    def LRTA_cost(self, s, a, s1, H):
-        """Returns cost to move from state 's' to state 's1' plus
-        estimated cost to get to goal from s1."""
-        if s1 is None:
-            return self.problem.h(s)
-        else:
-            # sometimes we need to get H[s1] which we haven't yet added to H
-            # to replace this try, except: we can initialize H with values from problem.h
-            if self.H.get(s1):
-                return self.problem.c(s, a, s1) + self.H[s1]
-            else:
-                return self.problem.c(s, a, s1)  + self.problem.h(s1)
+    # def LRTA_cost(self, s, a, s1, H):
+    #     """Returns cost to move from state 's' to state 's1' plus
+    #     estimated cost to get to goal from s1."""
+    #     if s1 is None:
+    #         return self.problem.h(s)
+    #     else:
+    #         # sometimes we need to get H[s1] which we haven't yet added to H
+    #         # to replace this try, except: we can initialize H with values from problem.h
+    #         if self.H.get(s1):
+    #             return self.problem.c(s, a, s1) + self.H[s1]
+    #         else:
+    #             return self.problem.c(s, a, s1)  + self.problem.h(s1)
